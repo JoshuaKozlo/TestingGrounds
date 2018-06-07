a3fd232bc5fa6ea0dcafd064fb14997a22aa639c
DESKTOP-GH3Q1KG\joshu <joshua.kozlo@gmail.com>
2018-06-06 22:10:35 -0400

    Attatch gun to FP Character

--- Source/TestingGrounds/Player/FirstPersonCharacter.cpp
+++ Source/TestingGrounds/Player/FirstPersonCharacter.cpp
@@ -10,6 +10,7 @@
 #include "MotionControllerComponent.h"
 #include "Engine/World.h"
 #include "Animation/AnimInstance.h"
+#include "../Weapons/Gun.h"
 #include "XRMotionControllerBase.h" // for FXRMotionControllerBase::RightHandSourceId
 
 DEFINE_LOG_CATEGORY_STATIC(LogFPChar, Warning, All);
@@ -64,8 +65,15 @@ void AFirstPersonCharacter::BeginPlay()
 	// Call the base class  
 	Super::BeginPlay();
 
+	if (GunBlueprint == NULL)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Missing a GunBlueprint"));
+		return;
+	}
+	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
+
 	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
-	//FP_Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
 
 	// Show or hide the two versions of the gun based on whether or not we're using motion controllers.
 	//if (bUsingMotionControllers)
