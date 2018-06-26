2736b87e4e938094cc9afc796189850d3e5f4ef6
DESKTOP-GH3Q1KG\joshu <joshua.kozlo@gmail.com>
2018-06-25 20:03:56 -0400

    Create pool for Nav Meshs

--- Source/TestingGrounds/Terrain/Tile.cpp
+++ Source/TestingGrounds/Terrain/Tile.cpp
@@ -3,6 +3,7 @@
 #include "Tile.h"
 #include "Engine/World.h"
 #include "DrawDebugHelpers.h"
+#include "ActorPool.h"
 
 
 // Sets default values
@@ -11,6 +12,29 @@ ATile::ATile()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	MinExtent = FVector(0, -2000, 0);
+	MaxExtent = FVector(4000, 2000, 0);
+
+}
+
+void ATile::SetPool(UActorPool* InPool)
+{
+	UE_LOG(LogTemp, Warning, TEXT("[%s] Setting Pool %s"), *(this->GetName()), *(InPool->GetName()));
+	Pool = InPool;
+
+	PositionNavMeshBoundsVolume();
+}
+
+void ATile::PositionNavMeshBoundsVolume()
+{
+	NavMeshBoundsVolume = Pool->Checkout();
+	if (NavMeshBoundsVolume == nullptr)
+	{
+		UE_LOG(LogTemp, Error, TEXT("[%s] Not enough actors in pool"), *GetName());
+		return;
+	}
+	//UE_LOG(LogTemp, Error, TEXT("[%s] Checked out: {%s}"), NavMeshBoundsVolume->GetName());
+	NavMeshBoundsVolume->SetActorLocation(GetActorLocation());
 }
 
 void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn, float Radius, float MinScale, float MaxScale)
@@ -31,9 +55,7 @@ void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn,
 
 bool ATile::FindEmptyLocation(FVector& OutLocation, float Radius)
 {
-	FVector Min(0, -2000, 0);
-	FVector Max(4000, 2000, 0);
-	FBox Bounds(Min, Max);
+	FBox Bounds(MinExtent, MaxExtent);
 	const int MAX_ATTEMPTS = 10;
 	for (size_t i = 0; i < MAX_ATTEMPTS; i++)
 	{
@@ -57,17 +79,14 @@ void ATile::PlaceActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint, float Ro
 	Spawned->SetActorScale3D(FVector(Scale));
 }
 
-// Called when the game starts or when spawned
 void ATile::BeginPlay()
 {
 	Super::BeginPlay();
 }
 
-// Called every frame
-void ATile::Tick(float DeltaTime)
+void ATile::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
-	Super::Tick(DeltaTime);
-
+	Pool->Return(NavMeshBoundsVolume);
 }
 
 bool ATile::CanSpawnAtLocation(FVector Location, float Radius)
--- Source/TestingGrounds/Terrain/Tile.h
+++ Source/TestingGrounds/Terrain/Tile.h
@@ -6,10 +6,23 @@
 #include "GameFramework/Actor.h"
 #include "Tile.generated.h"
 
+class UActorPool;
+
 UCLASS()
 class TESTINGGROUNDS_API ATile : public AActor
 {
+
 	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Sizing")
+	FVector MinExtent;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Sizing")
+	FVector MaxExtent;
 	
 public:	
 	// Sets default values for this actor's properties
@@ -18,15 +31,14 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Weapons")
 	void PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn = 1, int MaxSpawn = 1, float Radius = 500, float MinScale = 1, float MaxScale = 1);
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
 public:	
 	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Pool")
+	void SetPool(UActorPool* Pool);
 
 private:
+	void PositionNavMeshBoundsVolume();
 
 	bool FindEmptyLocation(FVector& OutLocation, float Radius);
 
@@ -34,6 +46,7 @@ private:
 
 	bool CanSpawnAtLocation(FVector Location, float Radius);
 
+	UActorPool* Pool;
 
-
+	AActor* NavMeshBoundsVolume;
 };
